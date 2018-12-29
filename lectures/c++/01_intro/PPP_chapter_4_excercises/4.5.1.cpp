2ef43290f5b84766b635e258cbcd3d3fcc742949
caterpillar85 <da.bruno.ita@gmail.com>
2018-12-29 20:03:27 +0100

    PPP 4.5.1

---
+++ lectures/c++/01_intro/PPP_chapter_4_excercises/4.5.1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+/* Prima del "main" si possono definire delle "funzioni" */
+
+/* Scrivere un programma che restituisca la radice quadrata  */
+/*
+int square(int x)
+{
+  return x*x;
+}
+
+int main() {
+
+std::cout << square(2) << '\n';
+std::cout << square(10) << '\n';
+*/
+
+
+/* Scrivere un programma che restituisca la radice quadrata senza usare moltiplicazioni,
+usando solo addizioni n volte*/
+int print_square(int input)
+{
+  int x1 = 0;
+  for (int i = input; i > 0; i--) {
+      x1 += input;
+  }
+  return x1;
+}
+
+int main() {
+
+int input;
+std::cout << "please, insert a value you want the square " << '\n';
+std::cin >> input;
+std::cout << "\nThe answer is: " << print_square(input) << '\n';
+
+}
