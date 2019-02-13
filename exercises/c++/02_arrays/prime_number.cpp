92fa167ed3f05786d21d39b0a2272c9010a97d23
caterpillar85 <da.bruno.ita@gmail.com>
2019-02-13 17:20:33 +0100

    ex lesson 2

---
+++ exercises/c++/02_arrays/prime_number.cpp
@@ -0,0 +1,59 @@
+/*
+    "Advanced Programming"
+    Target: Populate an array named `primes` containing the first prime numbers
+            up to 100. Consider 2 as the first prime (i.e. `primes[0] == 2`).
+    Author: Daniele Bruno
+    Version: 12/02/19
+*/
+
+#include <iostream>
+
+// template della funzione generica per la creazione di un array
+// restituisce il pointer al primo elemento
+template <typename Type>
+Type* array_template(int size)  //funzione di tipo "Type"
+{
+  Type* d_array{new Type[size]};
+  for(int i = 0; i < size; ++i)
+  {
+      d_array[i];
+  }
+  return d_array;
+}
+
+// Funzione (basata su template) per trovare i numeri primi
+template <typename Type>
+void print_array_primes(Type* d_array, int size)
+{
+// aggiungo elementi all'array facendo il check
+  int number = 0;
+  int* primes = array_template<int>(size);
+  for (int i = 2; i < size; i++) {
+    bool marker = true;
+    for (int j = 2; j < i; j++) {
+      if (i % j == 0) {
+        marker = false;
+        break;
+      }
+    }
+  if (marker) {
+    primes[number++] = i;
+    std::cout << "primes[" << i << "]" << " is prime number" << std::endl;
+    }
+  }
+      delete[] primes;
+}
+
+int main()
+{
+  std::cout << "This code allows to print an array of prime numbers" << std::endl;
+  std::cout << "Insert how many prime numbers do you want: " << std::endl;
+  int size = 0;
+  std::cin >> size;
+
+    // scrivo l'array richiamando il template
+    int* primes = array_template<int>(size);
+    print_array_primes(primes, size);
+
+  return 0;
+}
---
+++ exercises/c++/02_arrays/reverse_order.cpp
@@ -0,0 +1,60 @@
+/*
+    "Advanced Programming"
+    Target: Write a program that reads from stdin the length of an array and
+            then store in it the numbers given from stdin. The program should
+            then prints the numbers in **reverse order**.
+    Author: Daniele Bruno
+    Version: 09/02/19
+*/
+
+#include <iostream>
+
+// template della funzione generica per la creazione di un array
+// restituisce il pointer al primo elemento
+template <typename Type>
+Type* array_template(int size)  //funzione di tipo "Type"
+{
+  Type* d_array{new Type[size]};
+  for(int i = 0; i < size; ++i)
+  {
+      std::cout << "Enter the element number " << i + 1 << " of the array" << std::endl;
+      std::cin >> d_array[i];
+  }
+  return d_array;
+}
+
+// Funzione (basata su template) per invertire gli elementi di un array
+template <typename Type>
+void print_array_reverse(Type* d_array, int size)
+{
+      for(int i = size - 1; i >= 0; i--)
+    {
+        std::cout << "array[" << i << "] = " << d_array[i] << std::endl;
+    }
+    delete[] d_array;
+}
+
+int main()
+{
+  std::cout << "This code allows to reverse a given array" << std::endl;
+  std::cout << "Insert the array dimension: ";
+  int size;
+  std::cin >> size;
+  // scelta della funzione (integer or double)
+  std::cout << "Choose the type: 0 = integer ; 1 = double ";
+  int type{0};
+  std::cin >> type;
+  if (type == 0) {
+    // scrivo l'array richiamando il template
+    // array di tipo int
+    int* int_array = array_template<int>(size);
+    print_array_reverse(int_array, size);
+  }
+    else {
+      // scrivo l'array richiamando il template
+      // array di tipo double
+      double* double_array = array_template<double>(size);
+      print_array_reverse(double_array, size);
+    }
+  return 0;
+}
